c4d0dc6f80b9c8652c581f680e2579dd6efeab77
Michael Paulukonis <xraysmalevich@gmail.com>
2015-04-01 22:46:20 -0400

    hooked up to twitter, and it tweets

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ var Spew = require('./dprk.spew.js');
 var senttags = require('./tagged.slogans.js');
 var spewer = new Spew(require('./slogans.words.tagged.min.json'));
 var _ = require('underscore');
+var config = require('./config.js');
+var Twit = require('twit');
+var T = new Twit(config);
+
+
+
+var logger = function(msg) {
+  if (config.log) console.log(msg);
+};
+
 
 var cleanup = function(text) {
 
@@ -13,8 +23,7 @@ var cleanup = function(text) {
   // removes spaces before punctuation
   clean = clean.replace(/\s+([.,;!])/g, '$1');
 
-  // capitalize first word (leave all other caps alone
-  // should be fixed w/in .json
+  // capitalize first word (leave all other caps alone)
   clean = clean.charAt(0).toUpperCase() + clean.slice(1);
 
   return clean;
@@ -22,14 +31,53 @@ var cleanup = function(text) {
 };
 
 
-var st = _.sample(senttags);
-var tags = st[0];
-var sent = st[1];
+var tweeter = function() {
 
-console.log(sent + '\n' + tags);
+  try {
 
-var spewed = spewer.spew(tags);
-spewed = cleanup(spewed);
-// TODO: fix capitalization
+    var st = _.sample(senttags);
+    var tags = st[0];
+    var sent = st[1];
 
-console.log('\n' + spewed.length + ' : ' + spewed);
+    logger(sent + '\n' + tags);
+
+    var spewed = spewer.spew(tags);
+    spewed = cleanup(spewed);
+
+    logger('\n' + spewed.length + ' : ' + spewed);
+
+  } catch (err) {
+    console.log('Error: ' + err.message);
+  }
+
+  if (spewed.length === 0 || spewed.length > 140) {
+    tweeter();
+  } else {
+    if (config.tweet_on) {
+      T.post('statuses/update', { status: spewed }, function(err, reply) {
+	if (err) {
+	  console.log('error:', err);
+	}
+	else {
+          // nothing on success
+	}
+      });
+    }
+  }
+
+};
+
+
+// Tweets ever n minutes
+// set config.seconds to 60 for a complete minute
+setInterval(function () {
+  try {
+    tweeter();
+  }
+  catch (e) {
+    console.log(e);
+  }
+}, 1000 * config.minutes * config.seconds);
+
+// Tweets once on initialization.
+tweeter();
